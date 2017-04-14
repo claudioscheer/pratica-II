8ab360ff896d19e27b59df58ef01f128a63774a4
claud <claudioscheer16@gmail.com>
2017-04-14 01:46:32 -0300

    Cadastro de Período

--- src/main/java/br/org/gdt/beans/FpPeriodoBean.java
+++ src/main/java/br/org/gdt/beans/FpPeriodoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @SessionScoped
