90ac41e3cacd03a69c5f3caf37fe923552516372
Anderson Seibert <anderson_seibert@hotmail.com>
2017-04-23 11:41:51 -0300

    No Commit Message

--- src/main/java/br/org/gdt/beans/RecSelecaoBean.java
+++ src/main/java/br/org/gdt/beans/RecSelecaoBean.java
@@ -10,18 +10,53 @@ import javax.faces.bean.RequestScoped;
 @ManagedBean
 @RequestScoped
 public class RecSelecaoBean {
-    
+
+    private boolean formAtivo = false;
+
     private RecSelecao selecao = new RecSelecao();
     private List<RecSelecao> selecoes;
-    
+
     @ManagedProperty("#{recSelecaoService}")
     private RecSelecaoService recSelecaoService;
-    
+
     public RecSelecaoBean() {
     }
-    
-    public void Salvar(){
-        
+
+    public void Salvar() {
+        if (selecao.getRecIdselecao() > 0) {
+            recSelecaoService.Alterar(selecao);
+        } else {
+            recSelecaoService.Inserir(selecao);
+        }
+        selecoes = recSelecaoService.ListarTodas();
+    }
+
+    public List<RecSelecao> ListarTodas() {
+        if (selecoes == null) {
+            selecoes = recSelecaoService.ListarTodas();
+        }
+        return selecoes;
+    }
+
+    public String PreparaEdicao(RecSelecao selecao) {
+        this.formAtivo = true;
+        this.selecao = selecao;
+        return "selecao_lista";
+    }
+
+    public String Excluir(RecSelecao selecao) {
+        recSelecaoService.Excluir(selecao.getRecIdselecao());
+        return "vaga_lista";
+    }
+
+    public void Cancelar() {
+        this.formAtivo = false;
+        this.selecao = new RecSelecao();
+    }
+
+    public void Adicionar() {
+        this.formAtivo = true;
+        this.selecao = new RecSelecao();
     }
 
     public RecSelecao getSelecao() {
@@ -46,5 +81,13 @@ public class RecSelecaoBean {
 
     public void setRecSelecaoService(RecSelecaoService recSelecaoService) {
         this.recSelecaoService = recSelecaoService;
+    }
+
+    public boolean isFormAtivo() {
+        return formAtivo;
+    }
+
+    public void setFormAtivo(boolean formAtivo) {
+        this.formAtivo = formAtivo;
     }
 }
