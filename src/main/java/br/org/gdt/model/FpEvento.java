d4f8d77bf3c61e70f182145684f0732f05036c71
claud <claudioscheer16@gmail.com>
2017-05-01 01:32:06 -0300

    FP - Cadastro de eventos

--- src/main/java/br/org/gdt/model/FpEvento.java
+++ src/main/java/br/org/gdt/model/FpEvento.java
@@ -20,7 +20,7 @@ public class FpEvento implements java.io.Serializable {
     private FpTipoEvento eveTipoEvento;
     private boolean eveIncideINSS;
     private boolean eveIncideFGTS;
-    private boolean eveIncideIR;
+    private boolean eveIncideIRRF;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_fp_evento")
@@ -72,12 +72,12 @@ public class FpEvento implements java.io.Serializable {
         this.eveIncideFGTS = eveIncideFGTS;
     }
 
-    public boolean isEveIncideIR() {
-        return eveIncideIR;
+    public boolean isEveIncideIRRF() {
+        return eveIncideIRRF;
     }
 
-    public void setEveIncideIR(boolean eveIncideIR) {
-        this.eveIncideIR = eveIncideIR;
+    public void setEveIncideIRRF(boolean eveIncideIRRF) {
+        this.eveIncideIRRF = eveIncideIRRF;
     }
 
 }
