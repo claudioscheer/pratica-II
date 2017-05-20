548b1d52a997be2457e721bfb23c01c25d780a6e
joaooliveira121e <juaooliveira.lp@gmail.com>
2017-05-20 10:07:10 -0300

    sprint || finalizacao

--- src/main/java/br/org/gdt/model/RecRequisicaoVaga.java
+++ src/main/java/br/org/gdt/model/RecRequisicaoVaga.java
@@ -18,16 +18,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import org.codehaus.jackson.annotate.JsonIgnore;
 
 /**
  *
