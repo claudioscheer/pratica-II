1e01282ab11e56f6a190b6812eee6a0b8e33faff
Claudio Scheer <claudioscheer16@gmail.com>
2017-06-17 01:03:01 -0300

    Redirecionar para a página inicial se o usuário já estiver logado

--- src/main/java/br/org/gdt/beans/LoginBean.java
+++ src/main/java/br/org/gdt/beans/LoginBean.java
@@ -1,13 +1,12 @@
 package br.org.gdt.beans;
 
 import br.org.gdt.resources.Helper;
+import java.io.IOException;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @ManagedBean
 @ViewScoped
@@ -19,16 +18,28 @@ public class LoginBean {
     public LoginBean() {
     }
 
-    public String entrar() {
+    @PostConstruct
+    public void init() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        if (request.getRemoteUser() != null) {
+            try {
+                context.getExternalContext().redirect(request.getContextPath() + "/index.xhtml");
+            } catch (IOException e) {
+            }
+        }
+    }
+
+    public void entrar() {
         FacesContext context = FacesContext.getCurrentInstance();
         HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
         try {
             request.login(login, senha);
-            return "/index.xhtml";
-        } catch (ServletException e) {
+            context.getExternalContext().redirect(request.getContextPath() + "/index.xhtml");
+        } catch (Exception e) {
             Helper.mostrarNotificacao("Login", "Usuário ou senha incorretos.", "error");
         }
-        return "/login.xhtml";
     }
 
     public String getLogin() {
